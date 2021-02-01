bfb2716cadd1fda4cb34918a9146d8f295a50230
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2021-02-01 15:16:20 -0700

    styling changes

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,27 @@
 <template>
-  <div id="nav">
-    <router-link to="/">
-      Home
-    </router-link> |
-    <router-link to="/about">
-      About
-    </router-link>
-  </div>
-  <router-view />
+  <header>
+    <!-- <navbar /> -->
+    <button class="btn btn-info">
+      Click me
+    </button>
+  </header>
+  <main>
+    <router-view />
+  </main>
+  <footer class="container-fluid">
+    <div class="row">
+      <div class="col bg-primary text-center p-2 text-light">
+        <p>Made with <img class="text-img" src="./assets/img/logo.png" /> by Codeworks</p>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import './assets/scss/main.scss';
+
+.text-img {
+  height: 1em;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
